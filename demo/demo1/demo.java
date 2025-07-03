0654b70401e131cfde28a0ef0c743c4c5d208241
VanThanh34 <vvt1304@gmail.com>
2025-07-03 21:14:38 +0700

    demo

--- demo/demo1/demo.java
+++ demo/demo1/demo.java
@@ -3,7 +3,28 @@ package demo.demo1;
 public class demo {
     public static void main(String[] args) {
 //        demoooooo
+        /* //khai báo biến
+                int a;
+                byte tuoi;
+                float diemToan;
+                //khai báo nhiều biến 1 lúc cùng kiểu
+                int c,d,e,f;*/
+
+                //khởi tạo biến
+                int tuoiCon = 10;
+//                float diemVan = 7.5f;
+//                double diemAnh = 7.5;
+                System.out.println("tuổi con trước khi thay đổi = " +tuoiCon);
+                tuoiCon=15;
+                System.out.println("tuổi con sau khi thay đổi = " +tuoiCon);
+
+                //hằng số
+//                final int Dosoi=100;
+
+
+
+        }
 
         
-    }
 }
+
