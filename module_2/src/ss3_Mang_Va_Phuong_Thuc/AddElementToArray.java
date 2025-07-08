b94fc489a6e1e2590bd09e4deb27fb1cf54cf359
VanThanh34 <vvt1304@gmail.com>
2025-07-08 11:49:05 +0700

    bài tập chèn phần tử vào mảng

--- module_2/src/ss3_Mang_Va_Phuong_Thuc/AddElementToArray.java
+++ module_2/src/ss3_Mang_Va_Phuong_Thuc/AddElementToArray.java
@@ -12,13 +12,13 @@ public class AddElementToArray {
         int n = sc.nextInt();
         System.out.print("Vị trí bạn muốn chèn là: ");
         int x = sc.nextInt();
-        if(x <= -1 || x > arr.length){
+        if(x <= 0 || x > arr.length){
             System.out.println("Không chèn được phần tử vào mảng");
         }
         int[] newArr = new int[arr.length+1];
         if (x >= 0) System.arraycopy(arr, 0, newArr, 0, x);
         newArr[x] = n;
-        if (arr.length - (x + 1) >= 0) System.arraycopy(arr, x + 1 - 1, newArr, x + 1, arr.length - (x + 1));
+        if (arr.length - (x + 1) >= 0) System.arraycopy(arr, x, newArr, x + 1, arr.length - (x + 1));
         System.out.println("Mảng sau khi thay đổi là: " + Arrays.toString(newArr));
     }
 }
