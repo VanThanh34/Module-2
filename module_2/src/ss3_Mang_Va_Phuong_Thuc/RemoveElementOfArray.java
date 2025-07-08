8d6a4f5acf7d590fdebb2ddfe6b61bbff76052cc
VanThanh34 <vvt1304@gmail.com>
2025-07-08 13:31:45 +0700

    bài tập xóa phần tử trong mảng

--- module_2/src/ss3_Mang_Va_Phuong_Thuc/RemoveElementOfArray.java
+++ module_2/src/ss3_Mang_Va_Phuong_Thuc/RemoveElementOfArray.java
@@ -7,20 +7,27 @@ public class RemoveElementOfArray {
     public static void main(String[] args) {
         int[] arr = {10, 4, 6, 7, 8, 6, 0, 0, 0};
         System.out.println("Mảng hiện có: " + Arrays.toString(arr));
-        int n;
+
+        int[] arr1 =removeElement(arr);
+        System.out.println("Mảng sau khi thay đổi: " + Arrays.toString(arr1));
+
+    }
+    public static int[] removeElement(int[] arr){
         System.out.print("Nhập phần tử cần xóa: ");
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+        int n = sc.nextInt();
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == n) {
                 System.out.println("Vị trí của phần tử cần xóa là: " + i);
-                for (int j = i ; j < arr.length-1; j++) {
-                arr[j] = arr[j+1];
-                arr[arr.length-1] = 0;
+                for (int j = i; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
                 }
-                System.out.println("Mảng sau khi thay đổi: "+Arrays.toString(arr));
+                arr[arr.length - 1] = 0;
+                i--;
             }
         }
+        return arr;
     }
 }
+
 
