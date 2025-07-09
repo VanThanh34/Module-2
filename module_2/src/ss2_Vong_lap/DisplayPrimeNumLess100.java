0f19d6ee8d43f88a7c1d55810d65f226981f45c4
VanThanh34 <vvt1304@gmail.com>
2025-07-09 08:54:25 +0700

    sửa bài tập hiển thị snt bé hơn 100

--- module_2/src/ss2_Vong_lap/DisplayPrimeNumLess100.java
+++ module_2/src/ss2_Vong_lap/DisplayPrimeNumLess100.java
@@ -2,8 +2,9 @@ package module_2.src.ss2_Vong_lap;
 
 public class DisplayPrimeNumLess100 {
     public static void main(String[] args) {
-        boolean isPrime = true;
+        boolean isPrime;
         for (int n = 2; n < 100; n++) {
+            isPrime = true;
             for (int i = 2; i <= Math.sqrt(n); i++) {
                 if (n % i == 0) {
                     isPrime = false;
