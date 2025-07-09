a479863bdfb962246f982c158874fd2ea17409f8
VanThanh34 <vvt1304@gmail.com>
2025-07-09 10:00:18 +0700

    tính tổng ở cột xác định

--- module_2/src/ss3_Mang_Va_Phuong_Thuc/SumElementOfColumn.java
+++ module_2/src/ss3_Mang_Va_Phuong_Thuc/SumElementOfColumn.java
@@ -41,7 +41,7 @@ public class SumElementOfColumn {
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
-                System.out.print("Input value in [" + i + "][" + j + "]: ");
+                System.out.printf("Input value in [%d][%d]: ", i, j);
                 array[i][j] = sc.nextInt();
             }
         }
