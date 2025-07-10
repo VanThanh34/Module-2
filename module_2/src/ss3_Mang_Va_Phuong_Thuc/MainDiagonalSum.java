586cb848bde02166b7eb90baddf087c9689e4c95
VanThanh34 <vvt1304@gmail.com>
2025-07-10 13:35:32 +0700

    sửa bài tính đường chéo

--- module_2/src/ss3_Mang_Va_Phuong_Thuc/MainDiagonalSum.java
+++ module_2/src/ss3_Mang_Va_Phuong_Thuc/MainDiagonalSum.java
@@ -7,12 +7,9 @@ public class MainDiagonalSum {
         int[][] arr = TwoDimensionalArray.input2DArray();
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr[i].length; j++) {
-                if(i == j){
-                    sum += arr[i][j];
-                }
+                    sum += arr[i][i];
             }
-        }
         System.out.println("Sum of main diagona is: " +sum);
+        }
     }
-}
+
