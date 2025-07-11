8d7925241f7af595d6298e9e43c642ad25f062ab
VanThanh34 <vvt1304@gmail.com>
2025-07-11 10:22:42 +0700

    Demo geometric

--- module_2/src/ss5_AccessModifier_StaticMethod_StaticProperty/demo.java
+++ module_2/src/ss5_AccessModifier_StaticMethod_StaticProperty/demo.java
@@ -1,5 +1,15 @@
 package module_2.src.ss5_AccessModifier_StaticMethod_StaticProperty;
 
-public class demo {
-    
+class MyClass {
+    void method(int x, int y) {
+        x = 5;
+        y = 5;
+    }
+
+    public static void main(String[] args) {
+        MyClass o = new MyClass();
+        int x = 1, y = 1;
+        o.method(x, y);
+        System.out.printf("x=%d, y=%d", x, y);
+    }
 }
---
+++ module_2/src/ss6_Ke_Thua/Geometric.java
@@ -0,0 +1,124 @@
+package module_2.src.ss6_Ke_Thua;
+
+public class Geometric {
+    private String color = "White";
+    private String filled = null;
+    public Geometric(){
+    }
+    public Geometric(String color, String filled){
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFilled() {
+        return filled;
+    }
+
+    public void setFilled(String filled) {
+        this.filled = filled;
+    }
+    public String toString() {
+        return "created with \"" + color + "\" color and " +
+                (filled == null ? "no fill" : "filled with \"" + filled + "\" color");
+    }
+}
+ class Circle extends Geometric{
+    private double radius;
+    public Circle(){
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+     public double getRadius() {
+         return radius;
+     }
+
+     public void setRadius(double radius) {
+         this.radius = radius;
+     }
+     public double getArea(){
+        return Math.PI * radius * radius;
+     }
+     public double getPerimeter(){
+        return 2 * Math.PI * radius;
+     }
+     public double getDiameter(){
+        return 2 * radius;
+     }
+     public void printCircle(){
+         System.out.println("The " + getColor() + " circle with radius with the radius is " + getRadius());
+     }
+ }
+ class Rectangle extends Geometric{
+    private double width;
+    private double height;
+
+    public Rectangle(){
+    }
+    public Rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+
+    }
+
+     public double getWidth() {
+         return width;
+     }
+
+     public void setWidth(double width) {
+         this.width = width;
+     }
+
+     public double getHeight() {
+         return height;
+     }
+
+     public void setHeight(double height) {
+         this.height = height;
+     }
+     public double getArea(){
+        return width * height;
+     }
+     public double getPerimeter(){
+        return 2 * (width + height);
+     }
+ }
+class TestCircleRectangle{
+    public static void main(String[] args) {
+        Geometric circle = new Geometric("brown", "green");
+        Circle circle1 = new Circle();
+        circle.setFilled("black");
+        circle1.setRadius(1.8);
+        Circle circle2 = new Circle(1.5);
+        System.out.println();
+        circle1.printCircle();
+        System.out.println("A circle main " + circle.toString());
+        System.out.println("The radius is " + circle1.getRadius());
+        System.out.println("The area is " + circle1.getArea());
+        System.out.println("The diameter is " + circle1.getDiameter());
+        System.out.println("The perimeter is " + circle2.getPerimeter());
+        System.out.println();
+        Rectangle rectangle1 = new Rectangle(2, 4);
+        Rectangle rectangle2 = new Rectangle();
+        rectangle1.setFilled("red");
+        rectangle1.setColor("blue");
+        rectangle2.setWidth(3);
+        rectangle2.setHeight(5);
+        System.out.println("A rectangle " + rectangle1.toString());
+        System.out.println("Width is " + rectangle2.getWidth());
+        System.out.println("Height is " + rectangle2.getHeight());
+        System.out.println("The area is " + rectangle1.getArea());
+        System.out.println("The perimeter is " + rectangle1.getPerimeter());
+        System.out.println();
+
+    }
+}
