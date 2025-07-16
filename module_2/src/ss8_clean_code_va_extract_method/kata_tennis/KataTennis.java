4ba1bbe9d2dc50bbe6d7561a09b2d046c7de07cf
VanThanh34 <vvt1304@gmail.com>
2025-07-16 08:47:09 +0700

    Sửa bài tập KataTennis

--- module_2/src/ss8_clean_code_va_extract_method/kata_tennis/KataTennis.java
+++ module_2/src/ss8_clean_code_va_extract_method/kata_tennis/KataTennis.java
@@ -1,6 +1,12 @@
 package module_2.src.ss8_clean_code_va_extract_method.kata_tennis;
 
 public class KataTennis {
+
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+
     public static String getScore(String player1, String player2, int score1, int score2) {
         if (isDraw(score1, score2)) {
             return getDrawScore(score1);
@@ -21,19 +27,19 @@ public class KataTennis {
 
     private static String getDrawScore(int score) {
          return switch (score) {
-            case 0->"Love-All";
-            case 1->"Fifteen-All";
-            case 2-> "Thirty-All";
-            case 3->"Forty-All";
+            case ZERO ->"Love-All";
+            case ONE ->"Fifteen-All";
+            case TWO -> "Thirty-All";
+            case THREE ->"Forty-All";
             default-> "Deuce";
         };
     }
 
     private static String getAdvantageOrWin(int score1, int score2, String player1, String player2) {
         int diff = score1 - score2;
-        if (diff == 1) return "Advantage " + player1;
-        if (diff == -1) return "Advantage " + player2;
-        if (diff >= 2) return "Win for " + player1;
+        if (diff == ONE) return "Advantage " + player1;
+        if (diff == -ONE) return "Advantage " + player2;
+        if (diff >= TWO) return "Win for " + player1;
         return "Win for " + player2;
     }
 
@@ -43,16 +49,16 @@ public class KataTennis {
 
     private static String getScoreTerm(int score) {
          return switch (score) {
-            case 0 ->"Love";
-            case 1->"Fifteen";
-            case 2->"Thirty";
-            case 3-> "Forty";
+             case ZERO ->"Love";
+            case ONE ->"Fifteen";
+            case TWO ->"Thirty";
+            case THREE -> "Forty";
             default-> "";
         };
     }
 
     public static void main(String[] args) {
-        String result = KataTennis.getScore("Thành", "Tịnh", 3, 2);
+        String result = KataTennis.getScore("Thành", "Tịnh", THREE, TWO);
         System.out.println(result);
     }
 }
