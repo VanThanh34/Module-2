7f21d76c9895146745395bf7eea9af9fd71f5ebb
VanThanh34 <vvt1304@gmail.com>
2025-07-17 12:00:18 +0700

    bài tập LinkedList

--- module_2/src/ss10_dsa_list/bai_tap/linked_list/MyLinkedList.java
+++ module_2/src/ss10_dsa_list/bai_tap/linked_list/MyLinkedList.java
@@ -1,5 +1,6 @@
 package module_2.src.ss10_dsa_list.bai_tap.linked_list;
 
+
 public class MyLinkedList<E> {
     private Node<E> head;
     private int numNodes = 0;
@@ -13,115 +14,57 @@ public class MyLinkedList<E> {
         }
     }
 
-
-    public MyLinkedList(E data) {
-        head = new Node<>(data);
-        numNodes++;
+    // Phương thức khởi tạo không tham số
+    public MyLinkedList() {
     }
 
-
-    public void add(E element) {
-        Node<E> temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
+    // Thêm phần tử vào vị trí chỉ định
+    public void add(int index, E element) {
+        if (index < 0 || index > numNodes) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds");
         }
-        temp.next = new Node<>(element);
-        numNodes++;
-    }
 
-
-    public void addFirst(E element) {
         Node<E> newNode = new Node<>(element);
-        newNode.next = head;
-        head = newNode;
+
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+
         numNodes++;
-    }
-
-
-    public void addLast(E element) {
-        add(element);
     }
 
 
     public void remove(int index) {
-        if (index < 0 || index >= numNodes) throw new IndexOutOfBoundsException();
+        if (index < 0 || index >= numNodes) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds");
+        }
+
         if (index == 0) {
             head = head.next;
         } else {
             Node<E> prev = head;
-            for (int i = 0; i < index - 1; i++) prev = prev.next;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
             prev.next = prev.next.next;
         }
+
         numNodes--;
-    }
-
-
-    public boolean remove(Object o) {
-        if (head == null) return false;
-        if (o.equals(head.data)) {
-            head = head.next;
-            numNodes--;
-            return true;
-        }
-        Node<E> current = head;
-        while (current.next != null) {
-            if (o.equals(current.next.data)) {
-                current.next = current.next.next;
-                numNodes--;
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
-    }
-
-
-    public E get(int index) {
-        if (index < 0 || index >= numNodes) throw new IndexOutOfBoundsException();
-        Node<E> temp = head;
-        for (int i = 0; i < index; i++) temp = temp.next;
-        return temp.data;
     }
 
 
     public int size() {
         return numNodes;
     }
-
-    public MyLinkedList<E> clone() {
-        if (head == null) return null;
-        MyLinkedList<E> newList = new MyLinkedList<>(head.data);
-        Node<E> current = head.next;
-        while (current != null) {
-            newList.add(current.data);
-            current = current.next;
-        }
-        return newList;
-    }
-
-
-
-    public int indexOf(E o) {
-        Node<E> temp = head;
-        for (int i = 0; i < numNodes; i++) {
-            if (o.equals(temp.data)) return i;
-            temp = temp.next;
-        }
-        return -1;
-    }
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        Node<E> temp = head;
-        sb.append("[");
-        while (temp != null) {
-            sb.append(temp.data);
-            if (temp.next != null) sb.append(", ");
-            temp = temp.next;
-        }
-        sb.append("]");
-        return sb.toString();
-    }
-
 }
+
+
 
