7dbd92065976cd5de09272ab6715c1ef12bffe73
VanThanh34 <vvt1304@gmail.com>
2025-07-17 12:05:08 +0700

    Bài tập ArrayList

--- module_2/src/ss10_dsa_list/bai_tap/array_list/MyList.java
+++ module_2/src/ss10_dsa_list/bai_tap/array_list/MyList.java
@@ -1,8 +1,5 @@
 package module_2.src.ss10_dsa_list.bai_tap.array_list;
 
-
-import java.util.Objects;
-
 public class MyList<E> {
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
@@ -12,85 +9,48 @@ public class MyList<E> {
         elements = new Object[DEFAULT_CAPACITY];
     }
 
-    public MyList(int capacity) {
-        elements = new Object[capacity];
-    }
+    public void add(int index, E element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
 
-    public void add(E element) {
         if (size == elements.length) {
             grow();
         }
-        elements[size++] = element;
-    }
 
-    private void grow() {
-        int newCapacity = (int) (elements.length * 1.5f);
-        Object[] newElements = new Object[newCapacity];
-        System.arraycopy(elements, 0, newElements, 0, elements.length);
-        elements = newElements;
-    }
-
-    public E get(int index) {
-        if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
         }
-        return (E) elements[index];
+
+        elements[index] = element;
+        size++;
     }
 
     public E remove(int index) {
-        if (index >= size || index < 0) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
 
-
-        E removedElement = (E) elements[index];
-
+        E removed = (E) elements[index];
 
         for (int i = index; i < size - 1; i++) {
             elements[i] = elements[i + 1];
         }
 
-
         elements[size - 1] = null;
         size--;
 
-        return removedElement;
-    }
-    public void clear() {
-        for (int i = 0; i < size; i++) {
-            elements[i] = null;
-        }
-        size = 0;
-    }
-    public int indexOf(E o) {
-        for (int i = 0; i < size; i++) {
-            if (elements[i] == null) {
-                if (o == null) return i;
-            } else {
-                if (elements[i].equals(o)) return i;
-            }
-        }
-        return -1;
-    }
-    public int size(){
-        return size;
-    }
-    public MyList<E> clone() {
-        MyList<E> clonedList = new MyList<>(elements.length);
-        for (int i = 0; i < size; i++) {
-            clonedList.add((E) elements[i]);
-        }
-        return clonedList;
-    }
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("[");
-        for (int i = 0; i < size; i++) {
-            sb.append(elements[i]);
-            if (i < size - 1) sb.append(", ");
-        }
-        sb.append("]");
-        return sb.toString();
+        return removed;
     }
 
+    public int size() {
+        return size;
+    }
+
+    private void grow() {
+        int newCapacity = elements.length + (elements.length >> 1); // tăng 1.5 lần
+        Object[] newArr = new Object[newCapacity];
+        System.arraycopy(elements, 0, newArr, 0, elements.length);
+        elements = newArr;
+    }
 }
--- module_2/src/ss10_dsa_list/bai_tap/array_list/MyListTest.java
+++ module_2/src/ss10_dsa_list/bai_tap/array_list/MyListTest.java
@@ -1,22 +1,17 @@
 package module_2.src.ss10_dsa_list.bai_tap.array_list;
 
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class MyListTest {
     public static void main(String[] args) {
-        MyList<Integer> list = new MyList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        System.out.println(list.get(2));
-        list.remove(1);
-//        System.out.println(list.get(-1));
-        System.out.println(list.get(0));
-        System.out.println(list.get(1));
-        System.out.println("Size is: "+list.size());
-        MyList<Integer> list2 = list.clone();
-        System.out.println(list2);
+        MyList<String> list = new MyList<>();
+        list.add(0, "A");
+        list.add(1, "B");
+        list.add(2, "C");
+        list.remove(0);
+        String removed = list.remove(1);
+        System.out.println("Đã xóa phần tử: " + removed);
+        System.out.println("Size: " + list.size());
     }
 }
