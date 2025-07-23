3a3a09c686ccd1bf31d7a16a2b1361b6cdaf440d
VanThanh34 <vvt1304@gmail.com>
2025-07-23 14:46:34 +0700

    bài tập cài đặt sx chèn

---
+++ module_2/src/ss14_sap_xep/btap/InsertionSort1.java
@@ -0,0 +1,20 @@
+package module_2.src.ss14_sap_xep.btap;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertionSort1 extends InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number element of array: ");
+        int n = Integer.parseInt(sc.nextLine());
+        int [] arr = new int [n] ;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1) + " : ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array before sort: "+Arrays.toString(arr));
+        insertionSort(arr);
+        System.out.println("Array after sort:  "+Arrays.toString(arr));
+    }
+}
