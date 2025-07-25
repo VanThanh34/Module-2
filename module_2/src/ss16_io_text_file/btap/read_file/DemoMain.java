5cf714d48218345a61b3b1546d6dcb14ef757c53
VanThanh34 <vvt1304@gmail.com>
2025-07-25 14:31:48 +0700

    btap ss16

--- module_2/src/ss16_io_text_file/btap/read_file/DemoMain.java
+++ module_2/src/ss16_io_text_file/btap/read_file/DemoMain.java
@@ -1,2 +1,8 @@
-package module_2.src.ss16_io_text_file.btap.read_file;public class DemoMain {
+package module_2.src.ss16_io_text_file.btap.read_file;
+
+public class DemoMain {
+    public static void main(String[] args) {
+        ReadFile readFile = new ReadFile();
+        readFile.readFile();
+    }
 }
