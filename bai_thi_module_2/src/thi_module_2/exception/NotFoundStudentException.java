2cb13b3b2b168bfeb19067cb5dc233e56380a715
VanThanh34 <vvt1304@gmail.com>
2025-08-13 13:54:17 +0700

    fix

--- bai_thi_module_2/src/thi_module_2/exception/NotFoundStudentException.java
+++
@@ -1,7 +0,0 @@
-package thi_module_2.exception;
-
-public class NotFoundStudentException extends Exception {
-    public NotFoundStudentException(String message) {
-        super(message);
-    }
-}
--- bai_thi_module_2/src/thi_module_2/view/ViewMain.java
+++
@@ -1,7 +0,0 @@
-package thi_module_2.view;
-
-public class ViewMain {
-    public static void main(String[] args) {
-        MenuView.displayMenu();
-    }
-}
